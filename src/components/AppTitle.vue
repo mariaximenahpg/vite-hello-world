8356752bf0a0d825413fb17fb1c40179e369ef75
mariaximenahpg <federico.businessrico@gmail.com>
2022-12-01 09:32:49 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 class="text-center">{{ title }}</h1>
+</template>
+
+<style scoped></style>
